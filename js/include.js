1d2e455aa6001a73a8abba8a1c11a2d8a4429a9f
David <lopezd@wustl.edu>
2026-01-26 16:56:16 -0800

    Last minute fixes to RSVP deadline and footer placement.

--- js/include.js
+++ js/include.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
-            document.body.insertAdjacentHTML('beforeend', data);
+           Array.from(document.body.children).filter(element => element.tagName === 'MAIN')[0].insertAdjacentHTML('beforeend', data);
         })
         .catch(error => console.error('Error loading footer:', error));
 });
